88cac5381d976c9732b167e8dd7e0fe837d92902
Seth Hager <HagerPrograms>
2025-01-31 22:01:10 -0600

    fix: default to port 8000

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,10 @@ import routes from './routes'
 import errorHandler from './middleware/error-handling'
 import { user } from './middleware/user'
 import health from './health'
-import config from 'config' 
 
 require('dotenv').config('')
 
-const PORT = config.get('PORT') ?? 8000
+const PORT = 8000
 
 app.use(express.json())
 app.use(user)

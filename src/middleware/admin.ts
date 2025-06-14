971d2c7bad0971982b6795ad243156a59ecc9351
Seth Hager <hagerprograms@gmail.com>
2025-06-14 17:25:45 -0500

    fix: apikeys

--- src/middleware/admin.ts
+++ src/middleware/admin.ts
@@ -1,6 +1,7 @@
 //admin middleware
 import { NextFunction, Request, Response } from "express"
 
+//ip whitelisting
 const admin = async (req: Request, res: Response, next: NextFunction) => {
     if(req.user.role !== "ADMIN"){
         return res.status(401).send({error: "Access to this api has been denied."})
